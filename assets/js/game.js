e5b9d00a0264124ce3de2409d5d14cf078c8e103
AllyGarcia <allygarcia152@gmail.com>
2022-02-20 22:11:43 -0600

    update based on snapshots

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,61 +4,7 @@ var randomNumber = function (min, max) {
   return value;
 };
 
-var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
-  health: 100,
-  attack: 10,
-  money: 10,
-  reset: function () {
-    this.health = 100;
-    this.money = 10;
-    this.attack = 10;
-  }, //comma!
-  refillHealth: function (){
-    if (this.money >= 7){
-      window.alert("Refilling player's health by 20 for 7 dollars.");
-      this.health +=20;
-      this.money -= 7;
-  } else {
-      window.alert("You don't have enough money!");
-    }
-  }, //comma!
-  upgradeAttack: function (){
-    if (this.money >= 7){
-      window.alert("Upgrading player's attack by 6 for 7 dollars.");
-      this.attack += 6;
-      this.money -= 7;
-    }
-    else {
-     window.alert("You don't have enough money!");
-    }
-  }
-};
-
-var enemyInfo = [
-  {
-    name: "Roborto",
-    attack: randomNumber (10, 14)
-  },
-  {
-    name: "Amy Android",
-    attack: randomNumber (10, 14)
-  },
-  {
-    name: "Robo Trumble",
-    attack: randomNumber (10, 14)
-  }
-];
-
-// you can also log multiple vlaues at once like this
-// console.log(
-//   playerInfo.name, 
-//   enemyNames, 
-//   enemyNames.length, 
-//   enemyNames [0],
-//   enemyNames [3]
-// );
-
+// fight function
 var fight = function (enemy){
   //repeat and execute as long as the enemy-robot is alive
   while (playerInfo.health > 0 && enemy.health > 0) {
@@ -83,6 +29,7 @@ var fight = function (enemy){
 
     //remove enemy's health by subtracking the amount set in the player's attack var
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+
     enemy.health = Math.max(0, enemy.health - damage);
     
     console.log(
@@ -103,6 +50,8 @@ var fight = function (enemy){
     }
 
     //remove player's health by subtracting the amount set in the enemy's attack var
+    var damage = randomNumber(enemy.attack - 3, enemy.attack);
+
     playerInfo.health = Math.max(0, playerInfo.health - enemy.attack);
     console.log(
       enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
@@ -135,9 +84,6 @@ var startGame = function (){
 
       // reset enemy.health before starting new fight
       pickedEnemyObj.health = randomNumber(40, 60);
-
-      //use debugger to pause script from running and check what's going on at that moment in the code
-      //debugger; 
 
       //pass the pickedenemy.name variable's value into the fight function, where it will assume the value of the enemy.name parameter
       fight (pickedEnemyObj);
@@ -214,6 +160,65 @@ var shop = function () {
       break;
   }
 };
+// game functions end
+
+// player information 
+var playerInfo = {
+  name: window.prompt("What is your robot's name?"),
+  health: 100,
+  attack: 10,
+  money: 10,
+  reset: function () {
+    this.health = 100;
+    this.money = 10;
+    this.attack = 10;
+  }, //comma!
+  refillHealth: function (){
+    if (this.money >= 7){
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+      this.health +=20;
+      this.money -= 7;
+    } 
+    else {
+      window.alert("You don't have enough money!");
+    }
+  }, //comma!
+  upgradeAttack: function (){
+    if (this.money >= 7){
+      window.alert("Upgrading player's attack by 6 for 7 dollars.");
+      this.attack += 6;
+      this.money -= 7;
+    }
+    else {
+     window.alert("You don't have enough money!");
+    }
+  }
+};
+
+// enemy information
+var enemyInfo = [
+  {
+    name: "Roborto",
+    attack: randomNumber (10, 14)
+  },
+  {
+    name: "Amy Android",
+    attack: randomNumber (10, 14)
+  },
+  {
+    name: "Robo Trumble",
+    attack: randomNumber (10, 14)
+  }
+];
+
+// you can also log multiple vlaues at once like this
+console.log(
+   enemyInfo, 
+   enemyInfo[0], 
+   enemyInfo[0].name, 
+   enemyInfo[0]['attack']
+);
+
 // start the game when the page loads
 startGame ();
 
