5b57aac49e44f0cadae543d6264e242a30125ede
AllyGarcia <allygarcia152@gmail.com>
2022-02-20 23:24:56 -0600

    debugg null player name input

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,60 +4,6 @@ var randomNumber = function (min, max) {
   return value;
 };
 
-var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
-  health: 100,
-  attack: 10,
-  money: 10,
-  reset: function () {
-    this.health = 100;
-    this.money = 10;
-    this.attack = 10;
-  }, //comma!
-  refillHealth: function (){
-    if (this.money >= 7){
-      window.alert("Refilling player's health by 20 for 7 dollars.");
-      this.health +=20;
-      this.money -= 7;
-  } else {
-      window.alert("You don't have enough money!");
-    }
-  }, //comma!
-  upgradeAttack: function (){
-    if (this.money >= 7){
-      window.alert("Upgrading player's attack by 6 for 7 dollars.");
-      this.attack += 6;
-      this.money -= 7;
-    }
-    else {
-     window.alert("You don't have enough money!");
-    }
-  }
-};
-
-var enemyInfo = [
-  {
-    name: "Roborto",
-    attack: randomNumber (10, 14)
-  },
-  {
-    name: "Amy Android",
-    attack: randomNumber (10, 14)
-  },
-  {
-    name: "Robo Trumble",
-    attack: randomNumber (10, 14)
-  }
-];
-
-// you can also log multiple vlaues at once like this
-// console.log(
-//   playerInfo.name, 
-//   enemyNames, 
-//   enemyNames.length, 
-//   enemyNames [0],
-//   enemyNames [3]
-// );
 
 var fight = function (enemy){
   //repeat and execute as long as the enemy-robot is alive
@@ -125,25 +71,25 @@ var startGame = function (){
   playerInfo.reset ();
 
   for(var i = 0; i < enemyInfo.length; i++) {
+    console.log(playerInfo);
     //if player is still alive, keep fighting
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0, so it needs to have 1 added to it
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-
+      
       //pick new enemy to fight based on the index of the enemy.names array
       var pickedEnemyObj = enemyInfo[i];
 
       // reset enemy.health before starting new fight
       pickedEnemyObj.health = randomNumber(40, 60);
 
-      //use debugger to pause script from running and check what's going on at that moment in the code
-      //debugger; 
+      console.log(pickedEnemyObj);
 
       //pass the pickedenemy.name variable's value into the fight function, where it will assume the value of the enemy.name parameter
       fight (pickedEnemyObj);
 
       // if we're not at the last enemy in the array
-      if (playerInfo.health > 0 && i < enemyNames.length - 1) {
+      if (playerInfo.health > 0 && i < enemyInfo.length - 1) {
         // ask if player want to use the store before next round
         var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
 
@@ -214,6 +160,64 @@ var shop = function () {
       break;
   }
 };
+
+//function to set name
+var getPlayerName = function (){
+  var name = "";
+  // ADD LOOP HERE WITH PROMPT AND CONDITION
+  while (name === "" || name === null){
+    name = prompt ("What is your robot's name?");
+  }
+  console.log("Your robot's name is" + name);
+  return name;
+};
+
+var playerInfo = {
+  name: getPlayerName (),
+  health: 100,
+  attack: 10,
+  money: 10,
+  reset: function () {
+    this.health = 100;
+    this.money = 10;
+    this.attack = 10;
+  }, //comma!
+  refillHealth: function (){
+    if (this.money >= 7){
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+      this.health +=20;
+      this.money -= 7;
+  } else {
+      window.alert("You don't have enough money!");
+    }
+  }, //comma!
+  upgradeAttack: function (){
+    if (this.money >= 7){
+      window.alert("Upgrading player's attack by 6 for 7 dollars.");
+      this.attack += 6;
+      this.money -= 7;
+    }
+    else {
+     window.alert("You don't have enough money!");
+    }
+  }
+};
+
+var enemyInfo = [
+  {
+    name: "Roborto",
+    attack: randomNumber (10, 14)
+  },
+  {
+    name: "Amy Android",
+    attack: randomNumber (10, 14)
+  },
+  {
+    name: "Robo Trumble",
+    attack: randomNumber (10, 14)
+  }
+];
+
 // start the game when the page loads
 startGame ();
 
